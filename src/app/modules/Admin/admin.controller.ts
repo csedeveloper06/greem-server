f86038fba0cffb1f53ccca95971d509c50e573bf
unknown <you@example.com>
2025-05-02 07:01:57 +0600

    added filtering and searchTerm functionality for admin

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,10 +1,16 @@
 import { Request, Response } from "express";
 import { AdminServices } from "./admin.service";
+import pick from "../../../shared/pick";
+import { adminFilterableFields } from "./admin.constant";
 
 const getAllAdmin = async (req: Request, res: Response) => {
   try {
     //   console.log(req.query);
-    const result = await AdminServices.getAllAdminsFromDB(req.query);
+    const filters = pick(req.query, adminFilterableFields);
+    const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
+    console.log(options);
+
+    const result = await AdminServices.getAllAdminsFromDB(filters, options);
     res.status(200).json({
       success: true,
       message: "admins retrieved successfully!",
--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -1,35 +1,48 @@
-import { Prisma, PrismaClient } from "../../../generated/prisma";
+import { Prisma } from "../../../generated/prisma";
+import { paginationHelper } from "../../../helpers/pagination";
+import prisma from "../../../shared/Prisma";
+import { adminSearchableFields } from "./admin.constant";
 
-const prisma = new PrismaClient();
-
-const getAllAdminsFromDB = async (params: any) => {
+const getAllAdminsFromDB = async (params: any, options: any) => {
+  const { page, limit, skip } = paginationHelper.calculatePagination(options);
+  const { searchTerm, ...filterData } = params;
   const andConditions: Prisma.AdminWhereInput[] = [];
-
   if (params.searchTerm) {
     andConditions.push({
-      OR: [
-        {
-          name: {
-            contains: params.searchTerm,
-            mode: "insensitive",
-          },
+      OR: adminSearchableFields.map((field) => ({
+        [field]: {
+          contains: params.searchTerm,
+          mode: "insensitive",
         },
-        {
-          email: {
-            contains: params.searchTerm,
-            mode: "insensitive",
-          },
-        },
-      ],
+      })),
     });
   }
 
+  if (Object.keys(filterData).length > 0) {
+    andConditions.push({
+      AND: Object.keys(filterData).map((key) => ({
+        [key]: {
+          equals: filterData[key],
+        },
+      })),
+    });
+  }
   const whereConditions: Prisma.AdminWhereInput = {
     AND: andConditions,
   };
 
   const result = await prisma.admin.findMany({
     where: whereConditions,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? {
+            [options.sortBy]: options.sortOrder,
+          }
+        : {
+            createdAt: "desc",
+          },
   });
   return result;
 };
